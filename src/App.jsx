9436e1bb1e769fbc0c49564fc8a8d101cd712d95
Confetto22 <enockagyei1999@gmail.com>
2025-04-23 05:57:23 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-[4rem] text-[#fff700] font-bold">
+        {" "}
+        HOUSE OF MYSTERY CHAPEL INT'L REBRAND
+      </p>
+    </>
+  );
+}
+
+export default App;
